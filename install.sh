dc79f2a646654e9f0fb1839bbeeaa816331e2921
Naveen khatri <naveen1khatri@gmail.com>
2026-04-23 17:47:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-
+#!/bin/bash
+clear
+echo "Installing AV Downloader Pro..."
+if command -v pkg >/dev/null 2>&1; then
+    pkg update -y
+    pkg install python ffmpeg -y
+    pip install -r requirements.txt
+    termux-setup-storage
+else
+    sudo apt update
+    sudo apt install python3-pip ffmpeg -y
+    pip3 install -r requirements.txt
+fi
+echo "Install Complete"
